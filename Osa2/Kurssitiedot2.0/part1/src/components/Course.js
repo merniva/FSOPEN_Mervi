d8f65ae24162cf18074de39664b350a268fa02d2
Mervi <mervi_niva@outlook.com>
2020-09-14 17:39:30 +0300

    2.4 & 2.5 done

---
+++ Osa2/Kurssitiedot2.0/part1/src/components/Course.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const Part = (props) => (
+    <>
+      {props.course.map((c) => (
+        <p key={c.id}>
+          {" "}
+          {c.name} {c.exercises}{" "}
+        </p>
+      ))}
+    </>
+  );
+  
+  const Header = ({ course }) => <h2>{course.name}</h2>;
+  
+  const Content = ({ course }) => {
+    return (
+      <div>
+        <Part course={course.parts} />
+      </div>
+    );
+  };
+  
+  const Total = ({course}) => {
+    let initialValue = 0
+    return (
+    <h4>
+      Number of exercises: {course.parts.reduce(
+    (acc, value) => acc + value.exercises
+    , initialValue
+  )}
+    </h4>
+  );
+    }
+  
+  const Course = ({ course }) => (
+    <>
+      <Header course={course} /> <Content course={course} /> <Total course={course} />
+    </>
+  );
+
+export default Course
