de90acd2778e6ced66818e1645bfad618affeb35
Mervi <mervi_niva@outlook.com>
2020-09-28 12:01:08 +0300

    build done & proxy added

---
+++ Osa2/Puhelinluettelo/part2/build/precache-manifest.edda964db161c2f9fbc382272613fcd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13098c51be9795fbe09fedfb8b62bc73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5d7b49e03bdafee5808",
+    "url": "/static/css/main.ed047ca9.chunk.css"
+  },
+  {
+    "revision": "315be99d837f8ac85ebb",
+    "url": "/static/js/2.aeb8816a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.aeb8816a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5d7b49e03bdafee5808",
+    "url": "/static/js/main.e3c76298.chunk.js"
+  },
+  {
+    "revision": "4b4367ca4fbadb01e2d9",
+    "url": "/static/js/runtime-main.e5a25448.js"
+  }
+]);
