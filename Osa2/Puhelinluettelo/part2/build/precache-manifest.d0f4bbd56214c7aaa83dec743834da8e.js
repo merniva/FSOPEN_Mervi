4c62e734f288914232b5074dd310ec7bfb668e71
Mervi <mervi_niva@outlook.com>
2020-10-02 14:45:15 +0300

    project built

---
+++ Osa2/Puhelinluettelo/part2/build/precache-manifest.d0f4bbd56214c7aaa83dec743834da8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af7b136c781352a04b271231166cfb04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "467c4c258f8b5377b0aa",
+    "url": "/static/css/main.ed047ca9.chunk.css"
+  },
+  {
+    "revision": "315be99d837f8ac85ebb",
+    "url": "/static/js/2.aeb8816a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.aeb8816a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "467c4c258f8b5377b0aa",
+    "url": "/static/js/main.4c265495.chunk.js"
+  },
+  {
+    "revision": "4b4367ca4fbadb01e2d9",
+    "url": "/static/js/runtime-main.e5a25448.js"
+  }
+]);
